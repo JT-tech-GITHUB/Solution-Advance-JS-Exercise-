0e344f5abf8e19f6b24652e8c6af76ba033debb4
Nat <nat@MacBook-Air.attlocal.net>
2021-06-11 18:00:48 -0700

    Adding Bonus Question's Solution

--- Q1_Solution.js
+++ Q1_Solution.js
@@ -22,3 +22,24 @@ const arrOrganizer = (arr) => {
 let input = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
 console.log(arrOrganizer(input));
 //[[1,1,1,1], [2,2,2], 4, 5, 10, [20,20], 391, 392, 591]
+
+
+/** BONUS */
+const seperator = (arr) => {
+  let result = [];
+  let numArr = [];
+  let strArr = [];
+  arr.sort((a, b) => Number(a) - Number(b));
+  arr.forEach(ele => {
+    if (typeof ele === 'number') {
+      numArr.push(ele);
+    } else {
+      strArr.push(ele);
+    }
+  });
+  result.push(numArr, strArr)
+  return result;
+}
+let input = [1, "2", "3", 2];
+console.log(seperator(input));
+//[[1, 2], ["2", "3"]]
